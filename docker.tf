efce5075f0cd5ec7caff32ab0acba785ab104df4
kandyba <m.kandyba@comparus.de>
2023-04-28 17:03:01 +0200

    add to VCS

---
+++ docker.tf
@@ -0,0 +1,10 @@
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
+data "docker_image" "my-site" {
+  name = "shadowshotx/product-go-micro:latest"
+}
+resource "docker_container" "my-site" {
+  image = data.docker_image.my-site.name
+  name  = "my-site"
+}
